8ad06b3e4b7cd762f0fbca4732b904b9375ff003
David Genord II <david@xspond.com>
2010-02-19 15:36:25 -0500

    Cleaned up default spec setup

--- spec/paranoid_spec.rb
+++ spec/paranoid_spec.rb
@@ -5,64 +5,71 @@ LUKE = 'Luke Skywalker'
 
 describe Paranoid do
   before(:each) do
-    Sticker.delete_all
-    Place.delete_all
-    Android.delete_all
-    Person.delete_all
-    Component.delete_all
-
-    @luke = Person.create(:name => LUKE)
-    @r2d2 = Android.create(:name => 'R2D2', :owner_id => @luke.id)
-    @c3p0 = Android.create(:name => 'C3P0', :owner_id => @luke.id)
-
-    @r2d2.components.create(:name => 'Rotors')
-
-    @r2d2.memories.create(:name => 'A pretty sunset')
-    @c3p0.sticker = Sticker.create(:name => 'OMG, PONIES!')
-    @tatooine = Place.create(:name => "Tatooine")
-    @r2d2.places << @tatooine
+    # Sticker.delete_all
+    # Place.delete_all
+    # Android.delete_all
+    # Person.delete_all
+    # Component.delete_all
+    #
+    # @luke = Person.create(:name => LUKE)
+    # @r2d2 = Android.create(:name => 'R2D2', :owner_id => @luke.id)
+    # @c3p0 = Android.create(:name => 'C3P0', :owner_id => @luke.id)
+    #
+    # @r2d2.components.create(:name => 'Rotors')
+    #
+    # @r2d2.memories.create(:name => 'A pretty sunset')
+    # @c3p0.sticker = Sticker.create(:name => 'OMG, PONIES!')
+    # @tatooine = Place.create(:name => "Tatooine")
+    # @r2d2.places << @tatooine
   end
 
-  it 'should recognize a class as paranoid' do
-    Person.paranoid?.should be_false
-    Place.paranoid?.should be_true
-  end
+  describe 'basic functionality' do
+    before(:each) do
+      Place.delete_all
+      @tatooine = Place.create(:name => "Tatooine")
+    end
 
-  it 'should hide destroyed records' do
-    @tatooine.update_attribute('deleted_at', Time.now)
-    Place.first(:conditions => {:name => 'Tatooine'}).should be_nil
-  end
+    it 'should recognize a class as paranoid' do
+      Person.paranoid?.should be_false
+      Place.paranoid?.should be_true
+    end
 
-  it 'should reveal destroyed records when with_destroyed' do
-    @tatooine.update_attribute('deleted_at', Time.now)
-    Place.with_destroyed.first(:conditions => {:name => 'Tatooine'}).should_not be_nil
-  end
+    it 'should hide destroyed records' do
+      @tatooine.update_attribute('deleted_at', Time.now)
+      Place.first(:conditions => {:name => 'Tatooine'}).should be_nil
+    end
 
-  it 'should restore the destroyed record' do
-    @tatooine.update_attribute('deleted_at', Time.now)
+    it 'should reveal destroyed records when with_destroyed' do
+      @tatooine.update_attribute('deleted_at', Time.now)
+      Place.with_destroyed.first(:conditions => {:name => 'Tatooine'}).should_not be_nil
+    end
 
-    @tatooine = Place.with_destroyed.first(:conditions => {:name => 'Tatooine'})
-    @tatooine.restore
+    it 'should restore the destroyed record' do
+      @tatooine.update_attribute('deleted_at', Time.now)
 
-    Place.first(:conditions => {:name => 'Tatooine'}).should_not be_nil
-  end
+      @tatooine = Place.with_destroyed.first(:conditions => {:name => 'Tatooine'})
+      @tatooine.restore
 
-  it 'should soft delete paranoid records' do
-    @tatooine.destroy
+      Place.first(:conditions => {:name => 'Tatooine'}).should_not be_nil
+    end
 
-    record = Place.with_destroyed.first(:conditions => {:name => 'Tatooine'})
-    record.should_not be_nil
-    record.deleted_at.should_not be_nil
-  end
+    it 'should soft delete paranoid records' do
+      @tatooine.destroy
 
-  it 'should mark the record destroyed' do
-    @tatooine.destroy
-    @tatooine.destroyed?.should be_true
-  end
+      record = Place.with_destroyed.first(:conditions => {:name => 'Tatooine'})
+      record.should_not be_nil
+      record.deleted_at.should_not be_nil
+    end
 
-  it 'should set the deleted_field' do
-    @tatooine.destroy
-    @tatooine.deleted_at.should_not be_nil
+    it 'should mark the record destroyed' do
+      @tatooine.destroy
+      @tatooine.destroyed?.should be_true
+    end
+
+    it 'should set the deleted_field' do
+      @tatooine.destroy
+      @tatooine.deleted_at.should_not be_nil
+    end
   end
 
   describe 'for alternate field information' do
