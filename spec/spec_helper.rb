fb894a3a97d79fbf14cc117f66a381f960d13ed6
David Genord II <david@xspond.com>
2010-07-28 17:07:01 -0400

    Allow logging of the sql output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,15 @@ require 'paranoid'
 require 'yaml'
 require 'spec'
 
+$enabled = false
+
+ActiveSupport::Notifications.subscribe(/sql/i) do |*args|
+  if $enabled
+    event = ActiveSupport::Notifications::Event.new(*args)
+    puts "SQL #{event.duration}: #{event.payload[:sql]}"
+  end
+end
+
 def connect(environment)
   conf = YAML::load(File.open(File.dirname(__FILE__) + '/database.yml'))
   ActiveRecord::Base.establish_connection(conf[environment])
